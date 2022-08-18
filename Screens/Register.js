fda7a5f8b4b7e777f0f71ffefb17d84cc252ee66
Nuwan-Wi <nuwanwijeweera97@gmail.com>
2022-08-18 19:07:01 +0530

    Register page text input fields added

--- Screens/Register.js
+++ Screens/Register.js
@@ -15,21 +15,60 @@ function Register() {
         <Image source={ require('../assets/Menu.png')} style={styles.menu}/>
         <Image source={ require('../assets/projectlogomini.png')} style={styles.logomini}/> 
         <Text style={styles.head} >Ayurveda Medi App </Text>
-        <Text style={{fontSize:20, textAlign:'center',marginTop:'7%', marginBottom:'15%' }}>Register</Text>
+        <Text style={{fontSize:20, textAlign:'center',marginTop:'7%', marginBottom:'10%' }}>Register</Text>
 
         <StatusBar style="auto" />
         <View style={styles.textInput}>
-            <TextInput placeholder='Email' style={styles.TypeingText}></TextInput>
+            <TextInput placeholder='Register As' style={styles.TypeingText}></TextInput>
         </View>
-        <View style={styles.textInput}>
-            <TextInput placeholder='Passward' style={styles.TypeingText}></TextInput>
+       
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >Name</Text>
+            <TextInput placeholder='Moda Pawi' style={{ flex:2, fontSize:16}}></TextInput>
         </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+        
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >NIC</Text>
+            <TextInput placeholder='981234567V' style={{ flex:2, fontSize:16}}></TextInput>
+        </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+        
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >District</Text>
+            <TextInput placeholder='Allapurate' style={{ flex:2, fontSize:16}}></TextInput>
+        </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+        
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >Contact No</Text>
+            <TextInput placeholder='0760806879' style={{ flex:2, fontSize:16}}></TextInput>
+        </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+        
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >Email</Text>
+            <TextInput placeholder='modapawi98@gmail.com' style={{ flex:2, fontSize:16}}></TextInput>
+        </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+        
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >Passward</Text>
+            <TextInput placeholder='********' style={{ flex:2, fontSize:16}}></TextInput>
+        </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+          
+        <View style={{flex:3, flexDirection:'row', alignItems:'center', marginLeft:"10%", marginTop:'2%',}}>
+            <Text style={{flex:1, fontSize:16}} >Re-Enter Passward</Text>
+            <TextInput placeholder='********' style={{ flex:2, fontSize:16}}></TextInput>
+        </View>
+        <View style={{backgroundColor: '#000000', height: 2, width:'80%', marginLeft:'10%'}}></View>
+        
 
         <Pressable style={styles.Press} onPress={() => navigation.navigate('Home')}>
             <Text style={styles.ButtonText}>Register</Text>
         </Pressable>
-        <View style={{backgroundColor: '#000000', height: 3, width:'90%', marginLeft:'5%'}}></View>
-        
+                
       </ImageBackground>
       </SafeAreaView>
     );
